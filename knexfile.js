8190803532e6b78ab54aff5ebf2412130def6227
julie hawkins <julabi@gmail.com>
2018-02-12 10:01:30 -0700

    sets up knexfile, fixes node-sass-chokidar config

---
+++ knexfile.js
@@ -0,0 +1,39 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/garage_bin', 
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/garage_bin_test', 
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    }
+  }
+};
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+const environment = process.env.NODE_ENV || 'development';
+const config = require('./knexfile')[environment];
+const database = require('knex')(config);
+
+const requireHTTPS = (request, response, next) => {
+  if (request.headers['x-forwarded-proto'] !== 'https') {
+    return response.redirect('https://' + request.get('host') + request.url);
+  }
+  next();
+};
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(requireHTTPS)
+}
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.locals.title = 'garage-bin';
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
+
+module.exports = app;
