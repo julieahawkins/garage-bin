d0b781f8b4056fd8d9277f10d88782abccca7d18
julie hawkins <julabi@gmail.com>
2018-02-12 14:27:38 -0700

    populates garages from db on load

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,18 +3,40 @@ const fetchData = async () => {
   let garages = await fetch('/api/v1/garages');
   garages = await garages.json();
 
-  await console.log(garages);
-
   console.log('getting items')
   let items = await fetch('/api/v1/items');
   items = await items.json();
 
-  await console.log(items);
+  populateGarages(garages.garages, items.items);
 };
 
-const openDoor = () => {
-  setTimeout(() => ($('.garage-door').toggleClass('open')), 600);
+const populateGarages = (garages, items) => {
+  console.log(garages);
+  console.log(items);
+
+  garages.forEach(garage => {
+    appendGarage(garage);
+  });
+};
+
+const appendGarage = (garage) => {
+  console.log(garage.id)
+  $('.garages').append(
+    `<div class="garage">
+      <h3 class="garage-name">${garage.name}</h3>
+      <div class="garage-door"> 
+      </div>
+      <div class="shelves shelf-3"></div>
+      <div class="shelves shelf-2"></div>
+      <div class="shelves shelf-1"></div>
+    </div>`
+  )
+};
+
+function openDoor() {
+  console.log(this)
+  setTimeout(() => ($(this).toggleClass('open')), 600);
 };
 
 $(document).ready(fetchData());
-$('.garage-door').on('click', openDoor);
+$('.garages').on('click', '.garage-door', openDoor);
