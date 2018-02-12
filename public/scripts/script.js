786c720f35eb6f07e1893a7b5e31b5bac6cfab9d
julie hawkins <julabi@gmail.com>
2018-02-12 14:17:27 -0700

    fetches garages and items on document.ready

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,20 @@
+const fetchData = async () => {
+  console.log('getting garages')
+  let garages = await fetch('/api/v1/garages');
+  garages = await garages.json();
+
+  await console.log(garages);
+
+  console.log('getting items')
+  let items = await fetch('/api/v1/items');
+  items = await items.json();
+
+  await console.log(items);
+};
+
 const openDoor = () => {
-  console.log('open sesame')
   setTimeout(() => ($('.garage-door').toggleClass('open')), 600);
 };
 
+$(document).ready(fetchData());
 $('.garage-door').on('click', openDoor);
